b1be99aa09f11bcdca4b5c5e7585b35e27814fd4
Márcio Prando Teixeira <mprandot@gmail.com>
2020-04-04 12:06:08 -0300

    Adding static ListTile and cards

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,24 @@ void main() => runApp(
     appBar: AppBar(
       title: Text('Transferências'),
     ),
+    body: Column(
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.0'),
+            subtitle: Text('1000'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('200.0'),
+            subtitle: Text('1000'),
+          ),
+        ),
+      ],
+    ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {
         AlertDialog(content: Text('Oi', textDirection: TextDirection.ltr));
