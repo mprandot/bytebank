c4f0edd93d5e60f5ab2b2030ae710dde2bb7a6dd
Márcio Prando Teixeira <mprandot@gmail.com>
2020-05-08 20:52:17 -0300

    Modificando o tema do APP

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,15 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: ListaTransferencias(),
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blue[800],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary
+        )
       ),
+      home: ListaTransferencias()
     );
   }
 }
